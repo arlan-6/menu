a473191858eaa92d054a9b66254a46fc998c3c3b
arlan-6 <arlanhan1997@gmail.com>
2025-03-07 22:56:29 +0500

    Add Vercel Analytics integration to layout and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ProductCart } from "@/components/ProductCart";
-
+import { Analytics } from "@vercel/analytics/react"
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
@@ -27,7 +27,7 @@ export default function RootLayout({
 		<html lang="en" >
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			><Analytics/>
 				<ProductCart>{children}</ProductCart>
 			</body>
 		</html>
